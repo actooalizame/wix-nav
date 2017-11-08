0a372979ad1807df4f3f7e60dabcac19239395be
actooalizame <gndalmedo@gmail.com>
2017-11-08 03:39:53 -0300

    working meteor subs

--- screens/Items.js
+++ screens/Items.js
@@ -2,9 +2,9 @@ import Meteor, {createContainer} from 'react-native-meteor';
 import React from 'react';
 import MeteorListViewComponent from './meteorListView';
 
-Meteor.connect('ws://localhost:3000/websocket');//do this only once
+Meteor.connect('ws://192.168.1.84:3000/websocket');//do this only once
 
-export default Items = createContainer(ownProps => {
+export default Items = createContainer(props => {
   const itemsHandle = Meteor.subscribe('items');
   return {
       itemsReady: itemsHandle.ready()
